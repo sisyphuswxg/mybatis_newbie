03aed004877a410d81fd27c2ad96111d9333c26d
wangxg <wangxg@wangxgs-Mini.lan>
2020-02-03 11:17:20 +0800

    mybatis HelloWorld

---
+++ MyBatis_HelloWorld/src/com/atguigu/mybatis/dao/EmployeeMapperAnnotatioin.java
@@ -0,0 +1,11 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.bean.Employee;
+import org.apache.ibatis.annotations.Select;
+
+public interface EmployeeMapperAnnotatioin {
+
+	@Select("select * from tbl_employee where id=${id}")
+	public Employee getEmpById(Integer id);
+
+}
